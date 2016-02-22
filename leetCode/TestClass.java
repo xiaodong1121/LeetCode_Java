398345d2d0979d9e8bd31753b3c156850a68599c
xiaodong1121 <cxd19891121@yahoo.com>
2016-02-22 17:26:47 -0600

    upload test 6

--- leetCode/TestClass.java
+++ leetCode/TestClass.java
@@ -5,6 +5,11 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("upLoadTest");
+		System.out.println("UploadTest2");
+		System.out.println("UploadTest3");
+		System.out.println("UploadTest4");
+		System.out.println("UploadTest5");
+		System.out.println("UploadTest6");
 	}
 
 }
