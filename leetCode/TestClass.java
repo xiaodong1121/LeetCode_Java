d53ebe704dc35f097439ffdc1ec7a05a09533ed7
xiaodong1121 <cxd19891121@yahoo.com>
2016-02-22 17:44:42 -0600

    add new test: 8 9 10 11

--- leetCode/TestClass.java
+++ leetCode/TestClass.java
@@ -11,6 +11,10 @@ public class TestClass {
 		System.out.println("UploadTest5");
 		System.out.println("UploadTest6");
 		System.out.println("UploadTest7");
+		System.out.println("UploadTest8");
+		System.out.println("UploadTest9");
+		System.out.println("UploadTest10");
+		System.out.println("UploadTest11");
 	}
 
 }
