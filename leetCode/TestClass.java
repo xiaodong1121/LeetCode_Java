569d6a0e035881b9b0a5c0983f8954bb2af12368
xiaodong1121 <cxd19891121@yahoo.com>
2016-05-23 19:25:01 -0500

    all from start
    Delete all the information, let's just start from beginning.

--- leetCode/TestClass.java
+++ leetCode/TestClass.java
@@ -3,32 +3,9 @@ package leetCode;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("upLoadTest");
-		System.out.println("UploadTest2");
-		System.out.println("UploadTest3");
-		System.out.println("UploadTest4");
-		System.out.println("UploadTest5");
-		System.out.println("UploadTest6");
-		System.out.println("UploadTest7");
-		System.out.println("UploadTest8");
-		System.out.println("UploadTest9");
-		System.out.println("UploadTest10");
-		System.out.println("UploadTest11");
-		System.out.println("UploadTest12");
-		System.out.println("UploadTest13");
+		// TODO Auto-generated method stub	
+		System.out.println("Brand new test!");
 		
-		System.out.println("UploadTest14");
-		System.out.println("UploadTest15");
-		System.out.println("UploadTest16");
-		
-		// I just want to pull a request 
-		// is it possible?
-		
-		System.out.println("Pull request");
-		System.out.println("Pull request");
-		System.out.println("Pull request");
-		System.out.println("Pull request");
 	}
 
 }
